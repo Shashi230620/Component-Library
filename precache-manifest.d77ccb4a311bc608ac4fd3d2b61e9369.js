65f3d79b402b02e135ddb3adea31dcc019ad03f8
Shashi230620 <shashi.bhatt2306@gmail.com>
2024-08-23 20:39:57 +0530

    Updates

---
+++ precache-manifest.d77ccb4a311bc608ac4fd3d2b61e9369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d06bb558b7d5cfb01ce3b6567854e31",
+    "url": "/Component-Library/index.html"
+  },
+  {
+    "revision": "bd4b7ece47fdc2c12e30",
+    "url": "/Component-Library/static/css/main.92d67540.chunk.css"
+  },
+  {
+    "revision": "757320f153a6d60b07d1",
+    "url": "/Component-Library/static/js/2.1bff0bb4.chunk.js"
+  },
+  {
+    "revision": "cbf92ed71fb4f2e94692ba67215d89b7",
+    "url": "/Component-Library/static/js/2.1bff0bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd4b7ece47fdc2c12e30",
+    "url": "/Component-Library/static/js/main.d3b850a5.chunk.js"
+  },
+  {
+    "revision": "3a10dd86401f32aafefb",
+    "url": "/Component-Library/static/js/runtime-main.6a11915d.js"
+  }
+]);
