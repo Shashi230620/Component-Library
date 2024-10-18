8563d30e5235a1e1abfc91b7dd51f2525ce80c88
Shashi230620 <shashi.bhatt2306@gmail.com>
2024-10-18 23:49:52 +0530

    changes occures

--- src/Website/WebLayout/Navbar.jsx
+++ src/Website/WebLayout/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import navbaricon from "../assests/fav.png"
+import { Link } from "react-router-dom";
 const Navbar=()=>{
     return(<>
     <div className="Navbar">
         <div className="Navbar_Title">
         <img src={navbaricon} id="img" />
-          <h1 id="navbar_heading">Component Library</h1>
+          <Link to="/"style={{textDecoration:'none'}}><h1 id="navbar_heading">Component Library</h1></Link>
         </div>     
         <div className="Navbar_Buttons">
             <div className="Navbar_Search">
